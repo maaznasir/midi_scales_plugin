fb0a594eeb7fda96c04e41b508b8d42e3e6e80de
Maaz Nasir <maaz.nasir@gmail.com>
2021-01-02 21:21:40 -0500

    Scales Plugin Content Editor
    Ultimately this will evolve into the content editor that will be used for the  metadata and resources that will  be used by the scales plugin. Its a standalone tool that will exist on the side. For now, its just the empty project setup.

---
+++ MidiScalesContentEditor/Source/MainComponent.cpp
@@ -0,0 +1,29 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (800, 800);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setFont (juce::Font (16.0f));
+    g.setColour (juce::Colours::white);
+    g.drawText ("The tool that will be used as the content editor for the resources that will get packed into the scales plugin", getLocalBounds(), juce::Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
